868f5161adf4da59b7b425dc0eba3919c5ff441c
mariuspod <14898268+mariuspod@users.noreply.github.com>
2022-03-23 22:05:34 +0100

    feat: reducing the persisted joblib cache to the specified limit.

--- yearn/__init__.py
+++ yearn/__init__.py
@@ -1,9 +1,12 @@
 from brownie import network
 from yearn.logs import setup_logging
 from yearn.sentry import setup_sentry
+from yearn.cache import reduce_cache
 
 setup_logging()
 setup_sentry()
 if network.is_connected():
     from yearn.middleware.middleware import setup_middleware
     setup_middleware()
+
+reduce_cache()
--- yearn/cache.py
+++ yearn/cache.py
@@ -1,4 +1,9 @@
+import os
 from brownie import chain
 from joblib import Memory
 
-memory = Memory(f'cache/{chain.id}', verbose=0)
+CACHE_SIZE = int(os.getenv("CACHE_SIZE", 1<<30)) # default to 1 GB
+memory = Memory(f'cache/{chain.id}', verbose=0, bytes_limit=CACHE_SIZE)
+
+def reduce_cache():
+    memory.reduce_size()
