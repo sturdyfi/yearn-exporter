01c6da8f557095ac711f640bf951b277780799eb
mariuspod <14898268+mariuspod@users.noreply.github.com>
2022-03-28 10:27:45 +0200

    fix: use a string value for the bytes_limit.

--- yearn/cache.py
+++ yearn/cache.py
@@ -2,7 +2,7 @@ import os
 from brownie import chain
 from joblib import Memory
 
-CACHE_SIZE = int(os.getenv("CACHE_SIZE", 1<<30)) # default to 1 GB
+CACHE_SIZE = os.getenv("CACHE_SIZE", '1G') # default to 1 GB
 memory = Memory(f'cache/{chain.id}', verbose=0, bytes_limit=CACHE_SIZE)
 
 def reduce_cache():
