3dc2d07a97a1674e387b1ecdcf09a1c14c9e8cd0
mariuspod <14898268+mariuspod@users.noreply.github.com>
2022-03-28 22:59:10 +0200

    fix: decrease default cache to 100MB.

--- yearn/cache.py
+++ yearn/cache.py
@@ -2,7 +2,7 @@ import os
 from brownie import chain
 from joblib import Memory
 
-CACHE_SIZE = os.getenv("CACHE_SIZE", '1G') # default to 1 GB
+CACHE_SIZE = os.getenv("CACHE_SIZE", '100M') # default to 100 MB
 memory = Memory(f'cache/{chain.id}', verbose=0, bytes_limit=CACHE_SIZE)
 
 def reduce_cache():
